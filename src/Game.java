7da27fb9a3ce464afd5c227ede511b3e421bb264
Daniel Namaki <namaki.work@gmail.com>
2019-12-03 16:01:34 +0100

    Added a base game logic in Game class.
    Signed-off-by: Daniel <namaki.work@gmail.com>

--- src/Game.java
+++ src/Game.java
@@ -33,24 +33,38 @@ class Game implements Runnable {
         return;
     }
 
-    public int elaborateUserMove(String event, Player player) {
-        //useless controls?
-        /*if (player != currentPlayer) {
-            throw new IllegalStateException("Not your turn");
-        } else if (player.opponent == null) {
-            throw new IllegalStateException("You don't have an opponent yet");
-        } else {*/
-        //TODO: add here game logic
-        // interpret event, check table, update table, send result to users...
+    /**
+     * Function handling the game logic
+     * See README.md for more info about the protocol
+     * @param event command from the user
+     */
+
+    private void elaborateUserMove(String event) throws Exception{
+        if (event.startsWith("FIRE")){
+            if (String.valueOf(event.charAt(4)) == " " && event.length() == 9){
+                int x= Integer.parseInt(String.valueOf(event.charAt(5)+event.charAt(6)));
+                int y= Integer.parseInt(String.valueOf(event.charAt(7)+event.charAt(8)));
+                currentPlayer.fire(x, y);
+            } else throw new Exception();
+        }
+        else if (event.startsWith("SET")){
+            if (String.valueOf(event.charAt(3)) == " " && event.length() == 8){
+                int x= Integer.parseInt(String.valueOf(event.charAt(4)+event.charAt(5)));
+                int y= Integer.parseInt(String.valueOf(event.charAt(6)+event.charAt(7)));
+                currentPlayer.set(x, y);
+            } else throw new Exception();
+        }
+        else {
+            throw new Exception();
+        }
+
         System.out.println("EVENT: " + event);
-//        }
-        return 0;
     }
 
     private void manageGame() {
         while (clientsConnected()) {
             try {
-                //Loops until the move isn't invalid
+                //Loops until the move is valid
                 while (true) {
 
                     currentPlayer.output.println("MESSAGE Your move");
@@ -61,15 +75,17 @@ class Game implements Runnable {
                     System.out.print("Command received ---> ");
                     System.out.println(currentPlayer.name + ": " + command);
 
-                    //TODO: elaborate client input
-                    //If the move is not invalid then continue, otherwise repeat the command request
-                    if (!(elaborateUserMove(command, currentPlayer) == -1)) {
+                    //If the move is valid then continue, otherwise repeat the command request
+                    try {
+                        elaborateUserMove(command);
                         currentPlayer.output.println("You made your move: " + command);
                         opponent.output.println("Your opponent made his move: " + command);
                         break;
+                    } catch (Exception e) {
+                        currentPlayer.output.println("The move wasn't valid. You have to type another command...");
+                        opponent.output.println("The move wasn't valid. Your opponent is typing another command...");
                     }
                 }
-
                 //swap players and activate opponent turns
                 Player temp = currentPlayer;
                 currentPlayer = opponent;
